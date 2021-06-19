0ecd71ecec8de26906e6a7f46ec9fe9de9482848
PythonProjectML <84527030+PythonProjectML@users.noreply.github.com>
2021-06-19 20:53:35 +0530

    Update and rename README.md to app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+
+# It is a magic command to create a .py file
+import streamlit as st
+import joblib
+model = joblib.load('spam-ham')
+st.title('SPam Ham Classifier')
+ip = st.text_input("Enter the message")
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0]) # to remove one dimension we put 0
